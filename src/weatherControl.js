45935a8ae04225be4e743a3c5e670d5d8ff96aab
sinework <sine.s.amare@gmail.com>
2021-03-11 16:02:56 +0300

    add logic for fetching weather info and display

---
+++ src/weatherControl.js
@@ -0,0 +1,27 @@
+const populateWeather = (results) => {
+  const deg = document.querySelector('.degree');
+  const desc = document.querySelector('.description');
+  const city = document.querySelector('.location-name');
+  const img = document.querySelector('#icon-img');
+  city.innerHTML = results.name;
+  deg.innerHTML = results.main.temp;
+  desc.innerHTML = results.weather[0].description;
+  img.src = `http://openweathermap.org/img/w/${results.weather[0].icon}.png`;
+};
+const weatherFetcher = () => {
+  const inputBox = document.querySelector('.search-input');
+  const butn = document.querySelector('.btn');
+
+  butn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=metric&appid=a8d39b545ee760a37a11bbc7a1698ad6`,
+    )
+      .then((response) => response.json())
+      .then((data) => populateWeather(data))
+      .catch(() => alert('City does not exist!'));
+  });
+};
+
+export default weatherFetcher;
