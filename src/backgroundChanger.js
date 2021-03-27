faffb37d55e2a60a749d37cc1f8712c843c2d8e2
sinework <sine.s.amare@gmail.com>
2021-03-27 22:54:40 +0300

    Add toggle button and modularize the code

--- src/backgroundChanger.js
+++ src/backgroundChanger.js
@@ -1,28 +1,34 @@
+import clear from './assets/clear.jpg';
+import rain from './assets/rain.jpg';
+import cloudy from './assets/cloudy.jpg';
+import snow from './assets/snow.jpg';
+import storm from './assets/storm.jpg';
+
 const backgroundChanger = (results) => {
-    switch (results.weather[0].main) {
-      case 'Clear':
-        document.body.style.backgroundImage = `url(${clear})`;
-        break;
-      case 'Clouds':
-        document.body.style.backgroundImage = `url(${cloudy})`;
-        break;
-      case 'Rain':
-        document.body.style.backgroundImage = `url(${rain})`;
-        break;
-      case 'Drizzle':
-        document.body.style.backgroundImage = `url(${rain})`;
-        break;
-      case 'Mist':
-        document.body.style.backgroundImage = `url(${rain})`;
-        break;
-      case 'Thunderstorm':
-        document.body.style.backgroundImage = `url(${storm})`;
-        break;
-      case 'Snow':
-        document.body.style.backgroundImage = `url(${snow})`;
-        break;
-      default:
-        break;
-    }
-  };
-  export default backgroundChanger;
+  switch (results.weather[0].main) {
+    case 'Clear':
+      document.body.style.backgroundImage = `url(${clear})`;
+      break;
+    case 'Clouds':
+      document.body.style.backgroundImage = `url(${cloudy})`;
+      break;
+    case 'Rain':
+      document.body.style.backgroundImage = `url(${rain})`;
+      break;
+    case 'Drizzle':
+      document.body.style.backgroundImage = `url(${rain})`;
+      break;
+    case 'Mist':
+      document.body.style.backgroundImage = `url(${rain})`;
+      break;
+    case 'Thunderstorm':
+      document.body.style.backgroundImage = `url(${storm})`;
+      break;
+    case 'Snow':
+      document.body.style.backgroundImage = `url(${snow})`;
+      break;
+    default:
+      break;
+  }
+};
+export default backgroundChanger;
--- src/weatherControl.js
+++ src/weatherControl.js
@@ -1,38 +1,43 @@
-import clear from './assets/clear.jpg';
-import rain from './assets/rain.jpg';
-import cloudy from './assets/cloudy.jpg';
-import snow from './assets/snow.jpg';
-import storm from './assets/storm.jpg';
+import backgroundChanger from './backgroundChanger';
+import toggleTempUnit from './toggleTempUnit';
 
 const populateWeather = (results) => {
   const deg = document.querySelector('.degree');
+
   const desc = document.querySelector('.description');
   const city = document.querySelector('.location-name');
   const img = document.querySelector('#icon-img');
+  const tempSymbol = document.querySelector('.degree-symbol');
   city.innerHTML = results.name;
   deg.innerHTML = results.main.temp;
   desc.innerHTML = results.weather[0].description;
+  tempSymbol.textContent = 'F';
   img.src = `http://openweathermap.org/img/w/${results.weather[0].icon}.png`;
 };
-
 
 const weatherFetcher = () => {
   const inputBox = document.querySelector('.search-input');
   const butn = document.querySelector('.btn');
+  const city = document.querySelector('.location-name');
+  let tempData;
 
   butn.addEventListener('click', (e) => {
     e.preventDefault();
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=metric&appid=a8d39b545ee760a37a11bbc7a1698ad6`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=imperial&appid=a8d39b545ee760a37a11bbc7a1698ad6`,
     )
       .then((response) => response.json())
       .then((data) => {
-        populateWeather(data);
-        backgroundChanger(data);
+        tempData = data;
+        populateWeather(tempData);
+        backgroundChanger(tempData);
       })
-      .catch(() => alert('City does not exist!'));
+      .catch(() => { city.textContent = 'City does not exist!'; });
   });
+
+  const tglBtn = document.querySelector('.tglBtn');
+  tglBtn.addEventListener('click', toggleTempUnit, false);
 };
 
 export default weatherFetcher;
